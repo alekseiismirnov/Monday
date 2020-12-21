2318e9f6871e76e57fcd13022655b6b2b7f9ef21
Alekseii Smirnov <alekseii.sm@gmail.com>
2020-12-21 15:15:00 +0200

    queen_attac all positions

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,5 +1,7 @@
 class Array
   def queen_attack? position
-    self[1] == position[1]
+    self[1] == position[1] or
+      self[0] == position[0] or
+      (self[0] - position[0])^2 == (self[1] - position[1])^2
   end
 end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -9,4 +9,13 @@ describe('Array#queen_attack?') do
   it('is true, when horisontal is the same') do
     expect([1,8].queen_attack?([2, 8])).to(eq(true))
   end
+
+  it('is true, when vertical is the same') do
+    expect([5,3].queen_attack?([5, 4])).to(eq(true))
+  end
+
+  it('is true, when both are on same diagonal') do
+    expect([6,5].queen_attack?([2, 1])).to(eq(true))
+  end
+
 end
