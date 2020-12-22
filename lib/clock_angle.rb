0cd3da9083485fc0654c172514c4d418d8510ec4
Alekseii Smirnov <alekseii.sm@gmail.com>
2020-12-22 09:55:24 +0200

    clock_angle named magic numbers

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -6,13 +6,18 @@
 # We return the abs value from the difference.
 #
 
+HOUR_ANGLE = 30
+HOUR_MINUTE_ANGLE = 0.5 # one minute correction for hour hand
+MINUTE_ANGLE = 6
+HOUR_CYCLE = 12
+
 def clock_angle time
   hours = time.split(':')[0].to_f
-  hours = hours % 12
+  hours = hours % HOUR_CYCLE
 
   minutes = time.split(':')[1].to_f
 
-  angles = [hours*30 + minutes*0.5, minutes*6]
+  angles = [hours*HOUR_ANGLE + minutes*HOUR_MINUTE_ANGLE, minutes*MINUTE_ANGLE]
   angles_difference = angles.max - angles.min
 
   return angles_difference > 180 ? 360 - angles_difference : angles_difference
