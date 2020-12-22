66a76c152fe251bf6d9183979693e337e2b58817
Alekseii Smirnov <alekseii.sm@gmail.com>
2020-12-22 09:18:22 +0200

    clock_angle tests closed

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,1 +1,19 @@
-# Returns an angle between hour and minute hands for the particular time "HH:MM"
+# Returns an angle in *degrees* between hour and minute hands 
+#   for the particular time "HH:MM".
+# We would need to obtain 12 hours format.
+# Then, for hours angle would be hours*30 + minutes*0.5,
+#   for minutes minutes*6.
+# We return the abs value from the difference.
+#
+
+def clock_angle time
+  hours = time.split(':')[0].to_f
+  hours = hours % 12
+
+  minutes = time.split(':')[1].to_f
+
+  angles = [hours*30 + minutes*0.5, minutes*6]
+  angles_difference = angles.max - angles.min
+
+  return angles_difference > 180 ? 360 - angles_difference : angles_difference
+end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -1,2 +1,30 @@
 require 'rspec'
 require 'clock_angle'
+
+# Values for the test from here
+# https://rechneronline.de/winkel/clock-hands.php 
+#
+
+describe('clock_angle') do
+  it('returns right value for 00:00 ') do
+    expect(clock_angle("00:00")).to eq(0)
+  end
+  it('returns right value for round hours up to 12') do
+    expect(clock_angle("04:00")).to eq(120)
+    expect(clock_angle("05:00")).to eq(150)
+    expect(clock_angle("11:00")).to eq(30)
+  end
+
+  it('returns right value for round hours from 12') do
+    expect(clock_angle("12:00")).to eq(0)
+    expect(clock_angle("13:00")).to eq(30)
+    expect(clock_angle("19:00")).to eq(150)
+  end
+
+  it('correctly calculate time with minutes > 0') do
+    expect(clock_angle("12:01")).to eq(5.5)
+    expect(clock_angle("10:30")).to eq(135)
+    expect(clock_angle("21:12")).to eq(156)
+    expect(clock_angle("23:59")).to eq(5.5)
+  end
+end
